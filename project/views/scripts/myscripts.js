3b13fe47d422d493242ec240be4e2c23702619c0
osmith-CU <owsm6892@colorado.edu>
2021-07-08 18:54:13 -0600

    created script file and started txt file for question HTML read

---
+++ project/views/scripts/myscripts.js
@@ -0,0 +1,10 @@
+/*
+This document contains all the scripts for the user interaction with the question system.
+It reads values separated by a delimiter from a text file to populate the HTML and adjust the boostrap card deck
+*/
+
+var COUNT = 0;
+
+function questionAnswer(answer){
+    
+}
